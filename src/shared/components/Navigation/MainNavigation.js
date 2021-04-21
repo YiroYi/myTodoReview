75fd0fd239ce4582ac7c7b24e876c99d6439f4b2
YiroYi <yiroyi@live.com>
2021-04-20 23:12:39 -0500

    Add: sidedrawer to navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+  const[drawerIsOpen, setdrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setdrawerIsOpen(true);
+  }
+
+  const closeDrawer = () => {
+    setdrawerIsOpen(false);
+  }
+
   return (
-  <MainHeader>
-    <button className="main-navigation__menu-btn">
-      <span />
-      <span />
-      <span />
-    </button>
-    <h1 className="main-navigation__title">
-      <Link to="/">
-        Your Places.
-      </Link>
-    </h1>
-    <nav>
-      ...
-    </nav>
-  </MainHeader>
+  <React.Fragment>
+    {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+    {drawerIsOpen && <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer> /*&& means if this is true the render*/}
+    <MainHeader>
+      <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">
+          Your Places.
+        </Link>
+      </h1>
+      <nav className="main-navigation__header-nav">
+        <NavLinks />
+      </nav>
+    </MainHeader>
+  </React.Fragment>
   )
 }
 
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './NavLinks.css';
+import { NavLink } from 'react-router-dom';
+
 
 const NavLinks = props => {
-  return()
+  return(
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>ALL Users</NavLink>
+      </li>
+      <li>
+        <NavLink to="/u1/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">New Place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/auth">Authenticate</NavLink>
+      </li>
+    </ul>
+  )
 }
 
 export default NavLinks;
