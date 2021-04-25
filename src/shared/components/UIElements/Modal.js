83c58cfb4dcf8675bb2e3c7d777244b4f8929704
YiroYi <yiroyi@live.com>
2021-04-25 15:43:08 -0500

    Add: Styles to button

---
+++ src/shared/components/UIElements/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Modal.css';
+import ReactDOM from 'react-dom';
+
+const ModalOverlay = props => {
+
+}
+
+const Modal = props => {
+  return();
+}
+
+export default Modal;
