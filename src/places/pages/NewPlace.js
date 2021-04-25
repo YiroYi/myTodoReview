250b279d1460036b5f700f2b7df8e2ada731f9b2
YiroYi <yiroyi@live.com>
2021-04-25 17:23:07 -0500

    Adding: simple form file with css

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css';
+
 
 const NewPlace = () => {
-    return <h2>New Place Work</h2>
+    return <form className="place-form">
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+      />
+    </form>
 }
 
 export default NewPlace;
