c5744c1f6fad5cfb5737d66d77c4c5aa8219c80b
YiroYi <yiroyi@live.com>
2021-04-26 00:03:13 -0500

    Completing Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 
 import './NewPlace.css';
 
 
 const NewPlace = () => {
+    const titleInputHandler = useCallback(
+      (is, value, isValid) => {
+
+      }, []);
+
+    const descriptionInputHandler = useCallback(
+      (is, value, isValid) => {
+
+      }, []);
+
     return <form className="place-form">
       <Input
+        id="input"
         type="text"
         label="Title"
         element="input"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="textarea"
+        label="Description"
+        element="textarea"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid title"
+        onInput={descriptionInputHandler}
       />
     </form>
 }
