9683822216b7d01ad4cb6a434412f53404dff89e
YiroYi <yiroyi@live.com>
2021-05-02 23:25:07 -0500

    Add: places tesing data

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -15,12 +15,30 @@ const DUMMY_PLACES = [
   },
   {
     id: 'p2',
-    title: 'Empire State Building',
+    title: 'Busan city',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
     address: '20 W 34th St, New York, NY 10001',
     location: [-73.9878584, 40.7484405],
     creator: 'u2'
+  },
+  {
+    id: 'p3',
+    title: 'Mexico City',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: [-73.9878584, 40.7484405],
+    creator: 'u2'
+  },
+  {
+    id: 'p4',
+    title: 'Shanghai City',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: [-73.9878584, 40.7484405],
+    creator: 'u1'
   }
 ];
 
