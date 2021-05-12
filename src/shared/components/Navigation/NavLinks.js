c5e72c5ce6dadac90033dfe9eb65c7f79b9b667d
YiroYi <yiroyi@live.com>
2021-05-11 21:34:03 -0500

    Adding: logout state

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -25,6 +25,10 @@ const NavLinks = props => {
       <li>
         <NavLink to="/auth">Authenticate</NavLink>
       </li>)}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>)}
     </ul>
   )
 }
